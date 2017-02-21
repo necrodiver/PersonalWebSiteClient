75fe2875ca06812803470283de4a630e4cdbd200
necrodiver <neclodiver@live.com>
2017-02-21 17:57:42 +0800

    修改小部分内容，准备考虑是不是放弃angular2+typescript了，先用别的做

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    entry: './main.ts',
+    output: {
+        path: './dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+};
